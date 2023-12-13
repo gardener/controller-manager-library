58fba89fdd5b6c81eedee84b42c5f81686b6c7f8
Martin Weindel <martin.weindel@sap.com>
2023-12-13 14:35:12 +0100

    add more tooling

--- hack/tools.mk
+++ hack/tools.mk
@@ -7,13 +7,22 @@ KUBEBUILDER_K8S_VERSION  := 1.28.0
 KUBEBUILDER_TAG          := $(TOOLS_BIN_DIR)/kubebuilder
 KUBEBUILDER_DIR          := $(TOOLS_BIN_DIR)/kubebuilder_$(KUBEBUILDER_K8S_VERSION)
 KUBEBUILDER_ASSETS       := "$(shell realpath $(KUBEBUILDER_DIR))/bin"
-VGOPATH                  := $(TOOLS_BIN_DIR)/vgopath
 CONTROLLER_GEN           := $(TOOLS_BIN_DIR)/controller-gen
+GOLANGCI_LINT            := $(TOOLS_BIN_DIR)/golangci-lint
+GOIMPORTS                := $(TOOLS_BIN_DIR)/goimports
+GINKGO                   := $(TOOLS_BIN_DIR)/ginkgo
+VGOPATH                  := $(TOOLS_BIN_DIR)/vgopath
 
+GOLANGCI_LINT_VERSION ?= v1.55.1
 VGOPATH_VERSION ?= v0.1.3
+
+# Use this function to get the version of a go module from go.mod
+version_gomod = $(shell go list -mod=mod -f '{{ .Version }}' -m $(1))
 
 # tool versions from go.mod
 CONTROLLER_GEN_VERSION ?= $(call version_gomod,sigs.k8s.io/controller-tools)
+GINKGO_VERSION ?= $(call version_gomod,github.com/onsi/ginkgo/v2)
+GOIMPORTS_VERSION ?= $(call version_gomod,golang.org/x/tools)
 
 # Use this "function" to add the version file as a prerequisite for the tool target: e.g.
 #   $(HELM): $(call tool_version_file,$(HELM),$(HELM_VERSION))
@@ -28,6 +37,17 @@ $(TOOLS_BIN_DIR)/.version_%:
 
 $(CONTROLLER_GEN): $(call tool_version_file,$(CONTROLLER_GEN),$(CONTROLLER_GEN_VERSION))
 	go build -o $(CONTROLLER_GEN) sigs.k8s.io/controller-tools/cmd/controller-gen
+
+$(GOLANGCI_LINT): $(call tool_version_file,$(GOLANGCI_LINT),$(GOLANGCI_LINT_VERSION))
+	@# CGO_ENABLED has to be set to 1 in order for golangci-lint to be able to load plugins
+	@# see https://github.com/golangci/golangci-lint/issues/1276
+	GOBIN=$(abspath $(TOOLS_BIN_DIR)) CGO_ENABLED=1 go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANGCI_LINT_VERSION)
+
+$(GOIMPORTS): $(call tool_version_file,$(GOIMPORTS),$(GOIMPORTS_VERSION))
+	go build -o $(GOIMPORTS) golang.org/x/tools/cmd/goimports
+
+$(GINKGO): $(call tool_version_file,$(GINKGO),$(GINKGO_VERSION))
+	go build -o $(GINKGO) github.com/onsi/ginkgo/v2/ginkgo
 
 $(KUBEBUILDER_DIR): $(call tool_version_file,$(KUBEBUILDER_TAG),$(KUBEBUILDER_K8S_VERSION))
 	curl -sSL https://go.kubebuilder.io/test-tools/$(KUBEBUILDER_K8S_VERSION)/$(shell go env GOOS)/$(shell go env GOARCH) | tar -xvz
